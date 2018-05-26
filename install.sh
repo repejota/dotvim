20c913747b25a1e625f79fe2c6fb90db08636cba
Raül Pérez <repejota@gmail.com>
2018-05-26 23:42:31 +0200

    disable colorscheme

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/chriskempson/base16-vim.git ~/.vim/bundle/base-16.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
