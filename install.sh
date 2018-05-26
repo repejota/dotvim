b24800df1b419a6de4763bd3419aaaeba973f951
Raül Pérez <repejota@gmail.com>
2018-05-27 01:57:44 +0200

    check if it is already installed

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-git clone https://github.com/chriskempson/base16-vim.git ~/.vim/bundle/base-16.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -x ~/.vim/bundle/base-16.vim ]; then
+    git clone https://github.com/chriskempson/base16-vim.git ~/.vim/bundle/base-16.vim
+fi
+if [ -x ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 vim +PluginInstall +qall
