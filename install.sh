f2f84306823416a63550a1a18ba6d45a10f677e8
Raül Pérez <repejota@gmail.com>
2018-05-27 02:50:40 +0200

    updates

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-if [ ! -d ~/.vim/bundle/base-16.vim ]; then
+# if [ ! -d ~/.vim/bundle/base-16.vim ]; then
     git clone https://github.com/chriskempson/base16-vim.git ~/.vim/bundle/base-16.vim
-fi
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+#fi
+#if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
+#fi
 vim +PluginInstall +qall
