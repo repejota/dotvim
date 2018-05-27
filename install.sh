df3e8acc3ad04a4146699ea2679790ade5ad029b
Raül Pérez <repejota@gmail.com>
2018-05-27 02:07:14 +0200

    plugin install

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ fi
 if [ -x ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-vim +PluginInstall +qall
+
+#vim +PluginInstall +qall
